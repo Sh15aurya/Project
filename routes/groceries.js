75c5243c4535a3290f3fe8bd56b4560f9591764a
Sh15aurya <32080787+Sh15aurya@users.noreply.github.com>
2022-04-17 18:05:09 -0400

    Add files via upload
    Made some changes to groceries and index to fix minor errors.

--- routes/groceries.js
+++ routes/groceries.js
@@ -1,17 +1,17 @@
 var express = require('express');
-var router = express.Router();
+var app = express();
 var dummyData = require('../groceries_data_dummy.json')
 
-router.get('/fruits', function(req, res, next) {
+app.get('/fruits', function(req, res, next) {
   res.render('groceries', { items: dummyData.fruits, path: 'fruits' });
 });
 
-router.get('/vegetables', function(req, res, next) {
+app.get('/vegetables', function(req, res, next) {
   res.render('groceries', { items: dummyData.vegetables, path: 'vegetables' });
 });
 
-router.get('/frozen_foods', function(req, res, next) {
+app.get('/frozen_foods', function(req, res, next) {
   res.render('groceries', { items: dummyData.frozen_foods, path: 'frozen_foods' });
 });
 
-module.exports = router;
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,35 @@
 const express = require('express');
-const router = express.Router();
-const groceriesRouter = require('./groceries')
+var app = express();
+var groceriesRouter = require('./groceries')
 
 /* GET home page. */
-router.get("/", function (req, response) {
+app.get("/", function (req, response) {
   response.render("index", {Title:"DesiZon"});
 });
 
-router.use('/groceries', groceriesRouter);
+app.use('/groceries', groceriesRouter);
 
 
-router.get("/SignUp", function (req, response) {
+app.get("/SignUp", function (req, response) {
   response.render("SignUp", {Title:"DesiZon"});
 });
 
-router.get("/About", function (req, response) {
+app.get("/About", function (req, response) {
   response.render("About", {Title:"DesiZon"});
 });
 
-router.get("/schoolSupplies", function(req, response){
+app.get("/schoolSupplies", function(req, response){
   response.render("schoolSupplies",{Title: "DesiZon"});
 });
 
-router.get("/cart", function (req, response) {
+app.get("/cart", function (req, response) {
   response.render("cart", {Title:"DesiZon"});
 });
 
-router.get("/checkout", function (req, response) {
+app.get("/checkout", function (req, response) {
   response.render("checkout", {Title:"DesiZon"});
 });
 
 
 
-module.exports = router;
+module.exports = app;
